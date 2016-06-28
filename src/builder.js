96aaeb993a7c3bd45c0ba65aac51400875c1aaf4
Doug Johnson <finson@whidbey.com>
2016-06-27 23:47:49 -0700

    Builder is now a separate module.

--- src/builder.js
+++ src/builder.js
@@ -5,8 +5,10 @@
 // Doug Johnson, June 2016
 
 const EventEmitter = require("events");
-const log4js = require("log4js");
 
+const processManager = require('child_process');
+
+const log4js = require("log4js");
 const path = require("path");
 const thisModule = path.basename(module.filename,".js");
 const log = log4js.getLogger(thisModule);
@@ -15,7 +17,8 @@ log.setLevel('DEBUG');
 let builderSwitches = {
   hardware: '-hardware',
   tools: '-tools',
-  libraries: '-built-in-libraries'
+  libraries: '-built-in-libraries',
+  target: '-build-path'
 };
 
 let itemSwitches = {
@@ -36,11 +39,20 @@ class BldMan extends EventEmitter {
  * @param  {object} item   options that define the package being built and its target environment
  * @return {string}        the OS command string to run for the build
  */
-  buildCommand(action, item) {
+  composeBuilderCommand(action, item) {
 
     let partA = action.cmd;
+    if (typeof action.options === "object") {
+      for (let opt of Object.keys(action.options)) {
+        partA += ' -' + opt;
+        let value = action.options[opt];
+        if (typeof value === 'string' && value.length > 0) {
+          partA += '=' + value;
+        }
+      }
+    }
 
-    for (let s of ['hardware', 'tools', 'libraries']) {
+    for (let s of ['hardware', 'tools', 'libraries', 'target']) {
       let values = (Array.isArray(action[s])) ? action[s] : [action[s]];
       log.debug(`action: ${s} : ${values}`);
       for (let v of values) {
@@ -60,67 +72,24 @@ class BldMan extends EventEmitter {
         }
       }
     }
-
     partB += ' ' + item.sketch;
-
     return partA + partB;
+  }
+
+executeBuilderCommand(cmd) {
+  let child = processManager.exec(`${cmd}`, (error, stdout, stderr)  => {
+     if (error) {
+       console.log('Error code: '+error.code);
+       console.log(error.stack);
+     }
+     console.log('Child Process STDOUT: '+stdout);
+     console.log('Child Process STDERR: '+stderr);
+   });
+
+   child.on('exit', (code) => {
+     console.log('Child process exited with exit code '+code);
+   });
   }
 }
 
-
-// let hardwareA = '-hardware=C:/PROGRA~2/Arduino/hardware';
-// // let hardwareB = '-hardware=C:/Users/finson/AppData/Local/Arduino15/packages';
-// let hardwareB = '';
-// let toolsA = '-tools=C:/PROGRA~2/Arduino/hardware/tools/avr';
-// let toolsB = '-tools=C:/PROGRA~2/Arduino/tools-builder';
-// // let toolsC = '-tools=C:/Users/finson/AppData/Local/Arduino15/packages';
-// let toolsC = '';
-// let fqbn = '-fqbn=arduino:avr:uno';
-// // let builtInLibraries = '-built-in-libraries="C:/Program Files (x86)/Arduino/libraries"';
-// let builtInLibraries = '';
-
-// let buildPath = '-build-path=E:/users/finson/repos/batman/bldtgt';
-// // let libraries = '-libraries=E:/users/finson/repos/pipe/libraries';
-// let libraries = '';
-// let options = '-verbose=true';
-
-// let sketch = './bldsrc/Blink/Blink.ino';
-
-// let command = `arduino-builder ${options} ${hardwareA} ${hardwareB} ${toolsA} ${toolsB} ${toolsC} ${builtInLibraries} ${fqbn} ${libraries} ${buildPath} ${sketch}`;
-
-// let child = require('child_process').exec(`${command}`, (error, stdout, stderr)  => {
-//    if (error) {
-//      console.log(error.stack);
-// //     console.log('Error code: '+error.code);
-// //     console.log('Signal received: '+error.signal);
-//    }
-//    console.log('Child Process STDOUT: '+stdout);
-//    console.log('Child Process STDERR: '+stderr);
-//  });
-
-//  child.on('exit', (code) => {
-//    console.log('Child process exited with exit code '+code);
-//  });
-// }
-// }
-
-
-let buildable = {
-  name: 'uno-blinker',
-  board: 'arduino:avr:uno',
-  sketch: './bldsrc/Blink/Blink.ino',
-  libraries: '',
-  options: {}
-};
-
-let builder = {
-  cmd: "arduino-builder",
-  hardware: ['C:/PROGRA~2/Arduino/hardware'],
-  tools: ['C:/PROGRA~2/Arduino/hardware/tools/avr','C:/PROGRA~2/Arduino/tools-builder'],
-  libraries: [],
-  options: {}
-};
-
-let bat = new BldMan();
-let osCmd = bat.buildCommand(builder,buildable);
-log.info(osCmd);
+module.exports.BldMan = BldMan;
---
+++ src/run-builder.js
@@ -0,0 +1,34 @@
+// This module operates a parameterized build manager.
+//
+// This program is strict-mode throughout.
+//
+// Doug Johnson, June 2016
+
+const Builder = require("./builder");
+const log4js = require("log4js");
+const path = require("path");
+const thisModule = path.basename(module.filename,".js");
+const log = log4js.getLogger(thisModule);
+log.setLevel('DEBUG');
+
+let builder = {
+  cmd: "arduino-builder",
+  options: {'debug-level': '5'},
+  hardware: ['C:/PROGRA~2/Arduino/hardware'],
+  tools: ['C:/PROGRA~2/Arduino/hardware/tools/avr','C:/PROGRA~2/Arduino/tools-builder'],
+  libraries: [],
+  target: 'e:/users/finson/repos/batman/bldtgt'
+};
+
+let buildable = {
+  name: 'uno-blinker',
+  board: 'arduino:avr:uno',
+  sketch: './bldsrc/Blink/Blink.ino',
+  libraries: '',
+  options: {}
+};
+
+let bat = new Builder.BldMan();
+let osCmd = bat.composeBuilderCommand(builder,buildable);
+log.info(osCmd);
+bat.executeBuilderCommand(osCmd);
