65bda7666e460736c6a0d7b515da53cc8f11fd23
Doug Johnson <finson@whidbey.com>
2016-06-29 23:54:10 -0700

    Integrate (and refresh) Sequencer logic.

--- src/builder.js
+++ src/builder.js
@@ -14,14 +14,22 @@ const thisModule = path.basename(module.filename,".js");
 const log = log4js.getLogger(thisModule);
 log.setLevel('DEBUG');
 
-let builderSwitches = {
+let getActionSwitches = {
+  out: '-o'
+};
+
+let getItemSwitches = {
+
+};
+
+let bldActionSwitches = {
   hardware: '-hardware',
   tools: '-tools',
   libraries: '-built-in-libraries',
   target: '-build-path'
 };
 
-let itemSwitches = {
+let bldItemSwitches = {
   board: '-fqbn',
   libraries: '-libraries'
 };
@@ -33,13 +41,76 @@ class BldMan extends EventEmitter {
   }
 
 /**
+ * Given two objects containing various options describing what to get and how
+ * to get it, this method creates an array of commands to accomplish the get.
+ * @param  {object} action options that define the context for the getter
+ * @param  {object} item   options that define the packages to be copied
+ * @return {string}        the array of OS command strings to run for the getting
+ */
+  composeGetCommand(action,item) {
+
+    let partA = action.cmd;
+
+    // Simple options are copied directly to the command as switches
+
+    if (typeof action.options === "object") {
+      for (let opt of Object.keys(action.options)) {
+        partA += ' -' + opt;
+        let value = action.options[opt];
+        if (typeof value === 'string' && value.length > 0) {
+          partA += '=' + value;
+        }
+      }
+    }
+
+    // Action keys are translated to switches and added to the command
+
+    for (let s of []) {
+      let values = (Array.isArray(action[s])) ? action[s] : [action[s]];
+      log.debug(`action: ${s} : ${values}`);
+      for (let v of values) {
+        if (v.length > 0) {
+          partA += ' ' + getActionSwitches[s] + ' ' + v;
+        }
+      }
+    }
+
+    // Item keys are translated to switches and added to the command.
+
+    let partB = '';
+    for (let s of []) {
+      let values = (Array.isArray(item[s])) ? item[s] : [item[s]];
+      log.debug(`item: ${s} : ${values}`);
+      for (let v of values) {
+        if (v.length > 0) {
+          partB += ' ' + getItemSwitches[s] + '=' + v;
+        }
+      }
+    }
+
+    // The url to download is composed and added to the end of the command
+    // without a switch.
+
+
+    let result = [];
+    for (let r of item.repos) {
+      let partC = ' '+ item.repoRoot + '/' + action.user + '/' + r.name + '/zipball/master';
+      let partD = ' > ' +  action.out + '/' + r.name + '.zip';
+      result.push(partA+partB+partC+partD);
+      result.push('c:/PROGRA~1/7-zip/7z -y x ' + action.out + '/' + r.name + '.zip');
+    }
+    return result;
+  }
+
+
+/**
  * Given two objects containing various options for the build, this method
  * constructs the OS command to perform the build.
  * @param  {object} action options that define the context for the builder
  * @param  {object} item   options that define the package being built and its target environment
  * @return {string}        the OS command string to run for the build
  */
-  composeBuilderCommand(action, item) {
+  composeBuildCommand(action, item) {
 
     let partA = action.cmd;
     if (typeof action.options === "object") {
@@ -57,7 +128,7 @@ class BldMan extends EventEmitter {
       log.debug(`action: ${s} : ${values}`);
       for (let v of values) {
         if (v.length > 0) {
-          partA += ' ' + builderSwitches[s] + '=' + v;
+          partA += ' ' + bldActionSwitches[s] + '=' + v;
         }
       }
     }
@@ -68,7 +139,7 @@ class BldMan extends EventEmitter {
       log.debug(`item: ${s} : ${values}`);
       for (let v of values) {
         if (v.length > 0) {
-          partB += ' ' + itemSwitches[s] + '=' + v;
+          partB += ' ' + bldItemSwitches[s] + '=' + v;
         }
       }
     }
@@ -76,19 +147,16 @@ class BldMan extends EventEmitter {
     return partA + partB;
   }
 
-executeBuilderCommand(cmd) {
+executeOSCommand(cmd) {
   let child = processManager.exec(`${cmd}`, (error, stdout, stderr)  => {
      if (error) {
-       console.log('Error code: '+error.code);
-       console.log(error.stack);
+       this.emit("error",error);
      }
-     console.log('Child Process STDOUT: '+stdout);
-     console.log('Child Process STDERR: '+stderr);
    });
 
-   child.on('exit', (code) => {
-     console.log('Child process exited with exit code '+code);
-   });
+  child.on('exit', (code) => {
+      this.emit("exit",{status: code});
+    });
   }
 }
 
--- src/run-builder.js
+++ src/run-builder.js
@@ -4,14 +4,31 @@
 //
 // Doug Johnson, June 2016
 
-const Builder = require("./builder");
 const log4js = require("log4js");
 const path = require("path");
 const thisModule = path.basename(module.filename,".js");
 const log = log4js.getLogger(thisModule);
 log.setLevel('DEBUG');
 
-let builder = {
+const Sequencer = require("lib/Sequencer").Sequencer;
+const Builder = require("./builder");
+
+let getAction = {
+  cmd: 'c:/apps/curl-7.49.1/bin/curl',
+  user: 'finson',
+  out: './bldsrc/libraries',
+  options: {L:''}
+};
+
+let getItem = {
+  repoRoot: 'https://api.github.com/repos',
+  repos: [
+  {name:'Luni', branch: 'v0.10.0', release: 'latest'},
+  {name: 'FirmataWithDeviceFeature', branch: 'v0.10.0', release: 'latest'}
+  ]
+};
+
+let bldAction = {
   cmd: "arduino-builder",
   options: {'debug-level': '5'},
   hardware: ['C:/PROGRA~2/Arduino/hardware','C:/Users/finson/AppData/Local/Arduino15/packages'],
@@ -21,7 +38,7 @@ let builder = {
   target: 'e:/users/finson/repos/batman/bldtgt'
 };
 
-let buildable = {
+let bldItem = {
   name: 'uno-blinker',
   board: 'arduino:avr:uno',
   sketch: './bldsrc/Blink/Blink.ino',
@@ -30,6 +47,36 @@ let buildable = {
 };
 
 let bat = new Builder.BldMan();
-let osCmd = bat.composeBuilderCommand(builder,buildable);
-log.info(osCmd);
-bat.executeBuilderCommand(osCmd);
+
+let seq = new Sequencer(bat,["exit"]);
+log.trace(`Sequencer is created.`);
+
+seq.on("error", (apiError) => {
+  log.error(`Error: ${apiError}`);
+});
+
+seq.on("done", (apiResult) => {
+  log.info(`Steps completed.`);
+});
+
+function makeStepFunction(c) {
+  return function(apiResult) {
+    log.debug(`step result: ${apiResult.status}`);
+    log.debug(`next step: ${c}`);
+    bat.executeOSCommand(c);
+  };
+}
+
+let osCmd = bat.composeGetCommand(getAction,getItem);
+let step = [];
+for (let c of osCmd) {
+  log.trace(c);
+  step.push(makeStepFunction(c));
+}
+
+seq.start(step);
+
+
+// osCmd = bat.composeBuildCommand(bldAction,bldItem);
+// log.info(osCmd);
+// bat.executeOSCommand(osCmd);
